ce84dafe6c6070e6ee872b09077a5a69d273e58b
Immanuel <nowpertimmanuel@gmail.com>
2023-03-27 20:41:49 -0700

    Add project files.

---
+++ HandsOn2703/MaximizeProfit.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOn2703
+{
+    internal class MaximizeProfit
+    {
+
+        public static int FindMaxProfitEfficient(int days, List<int> stockPrices)
+        {
+            //O(days)
+            int maxProit = 0, smallestBuySoFar = stockPrices[0];
+
+            for(int i = 0; i < days; i++)
+            {
+            
+                maxProit = (stockPrices[i] - smallestBuySoFar > maxProit)? stockPrices[i]-smallestBuySoFar:maxProit;
+                
+
+                smallestBuySoFar = (stockPrices[i] < smallestBuySoFar)? stockPrices[i]:smallestBuySoFar;
+            }
+
+            return maxProit;
+        }
+     
+    public static int FindMaxProfit(int days, List<int> stockPrices)
+        {
+            //O(days^2)
+            int maxProfit = 0;
+
+
+            for(int buyDayIter= 0; buyDayIter < days;buyDayIter++)
+            {
+                int buyDayPrice = stockPrices[buyDayIter];
+
+                for(int sellDayIter=buyDayIter+1;sellDayIter < days; sellDayIter++)
+                {
+                    int profitPossible = stockPrices[sellDayIter]-buyDayPrice;
+                    maxProfit=(profitPossible>maxProfit)?profitPossible:maxProfit;
+                }
+            }
+
+
+            return maxProfit;
+
+        }
+    public static void Main(string[] args)
+    {
+
+            int days = Convert.ToInt32(Console.ReadLine());
+            List<int>stockPrices= new List<int>();
+
+            for(int i = 0; i < days; i++)
+            {
+                stockPrices.Add(Convert.ToInt32(Console.ReadLine()));   
+            }
+
+            Console.WriteLine("maximum profit you can achieve is "+FindMaxProfitEfficient(days, stockPrices));
+    }
+
+
+    }
+
+}
---
+++ HandsOn2703/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+
+namespace HandsOn2703 { 
+
+
+
+    public class HandsOn2704
+    {
+
+    //    public static void Main(string[] args)
+    //    {
+    //        int rows=Convert.ToInt32(Console.ReadLine()), cols=Convert.ToInt32(Console.ReadLine());
+
+    //        List<List<int>> matrix = new();
+
+    //        for(int i=0;i<rows;i++)
+    //        {
+    //            matrix.Add(new List<int>());
+    //            for(int j= 0; j < cols; j++)
+    //            {
+    //                matrix[i].Add(Convert.ToInt32(Console.ReadLine()));
+    //            }    
+    //        }
+
+    //        int totalCovered = 0,rowIndex=0,colIndex=cols-1;
+
+    //        while(totalCovered < rows* cols) {
+    //            //right
+    //            for(int i = rowIndex; i <= colIndex; i++)
+    //            {
+    //                Console.Write(matrix[rowIndex][i]+" ");  
+    //                totalCovered++;
+    //            }
+    //            //down
+    //            for(int i=rowIndex+1; i <= colIndex; i++)
+    //            {
+    //                Console.Write(matrix[i][colIndex] + " ");;
+    //                totalCovered++;
+    //            }
+    //            //left
+    //            for(int i=colIndex-1;i>=rowIndex;i--){
+    //                Console.Write(matrix[colIndex][i] +" ");
+    //                totalCovered++;
+    //            }
+    //            //up
+    //            for(int i = colIndex - 1; i >= rowIndex+1; i--)
+    //            {
+    //                Console.Write(matrix[i][rowIndex] + " ");
+    //                totalCovered++; 
+    //            }
+    //            rowIndex++;
+    //            colIndex--;
+    //        }
+    //    }
+    }
+}
